c4c91b2dc303c63ed2120c3fe54f88eded42f738
jackdaniel-jd <vigopattaraphong@gmail.com>
2025-11-08 14:39:03 +0700

    Week 11

--- src/app/api/word/route.ts
+++ src/app/api/word/route.ts
@@ -2,7 +2,9 @@ import { NextResponse } from "next/server";
 import { words } from "@/data/words";
 
 export async function GET() {
-  const randomIndex = Math.floor(Math.random() * words.length);
-  const word = words[randomIndex];
-  return NextResponse.json({ word });
+  // const randomIndex = Math.floor(Math.random() * words.length);
+  // const word = words[randomIndex];
+  const response = await fetch('http://localhost:8000/api/word');
+  const data = await response.json();
+  return NextResponse.json({ data });
 }
